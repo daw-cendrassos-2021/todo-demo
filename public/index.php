5c6737b31c40282eaf2439366954ad657418153b
Dani Prados <daniprados@gmail.com>
2020-08-30 19:40:30 +0200

    Todo-demo esquelet de la app guardant les dades amb cookies sense utilitzar model de dades.
    	modified:   public/index.php
    	new file:   public/view.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
 <?php 
 
-echo "Hola Món!!!!";
+$tasquesJson = $_COOKIE["tasques"];
+
+if(isset($tasquesJson)){
+	$tasques = json_decode($tasquesJson);
+} else {
+	$tasques= array();
+}
+
+$tasca = $_POST["tasca"];
+$delete = $_GET["delete"];
+
+if(isset($tasca)){
+	$tasques[] = $tasca;
+}
+
+if(isset($delete)){
+	array_splice ($tasques, $delete, 1);
+}
+
+setcookie("tasques",json_encode($tasques));
+
+include "view.php";
+
+
+

fcdc702fb3efdfb034cff003e1d76b5db0c0b4f3
Dani Prados <daniprados@gmail.com>
2020-08-31 21:27:50 +0200

    Todo app

--- public/view.php
+++ public/view.php
@@ -8,12 +8,12 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Todo APP</title>
   </head>
   <body>
 
     <nav class="navbar navbar-light bg-light sticky-top">
-      <a class="navbar-brand">TODO</a>
+      <a class="navbar-brand">Todo APP</a>
       <form class="form-inline" action="index.php" method="POST">
         <input class="form-control mr-sm-1" type="text" placeholder="Afegir tasca" aria-label="Afegir">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Afegir</button>
